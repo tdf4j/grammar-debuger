7e62798f9e879a42837c6f494bd3c690ada0d106
StabOnes <stab312@gmail.com>
2019-10-07 21:07:39 +0300

    Basic UI + fixed POM to properly assemble project

--- src/main/java/org/tdf4j/debugger/JavaFXLauncher.java
+++ src/main/java/org/tdf4j/debugger/JavaFXLauncher.java
@@ -3,6 +3,7 @@ package org.tdf4j.debugger;
 public class JavaFXLauncher {
     public static void main(String[] args) {
         //todo Что за еб*ный костыль и как он работает??? ._.
+        // Работает и ладно
         JavaFXApplication.main(args);
     }
 }
--- src/main/java/org/tdf4j/debugger/controller/MainController.java
+++ src/main/java/org/tdf4j/debugger/controller/MainController.java
@@ -2,17 +2,24 @@ package org.tdf4j.debugger.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MainController {
 
     @FXML
-    private Label labwork;
+    private TextArea Grammartextarea;
+    @FXML
+    private TextArea Textareatext;
+    @FXML
+    private TextArea Consoletextarea;
 
     @FXML
     public void onClickLoad() {
-        labwork.setText("Test work");
+        Grammartextarea.setText("Should");
+        Textareatext.setText("work");
+        Consoletextarea.setText("like this");
     }
 
 }
